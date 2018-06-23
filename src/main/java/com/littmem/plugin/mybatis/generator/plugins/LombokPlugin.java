db14cb6a5b9591e30bde9ec1b2d25949bef69628
ailinal <ailinal@yahoo.com>
2018-06-23 20:56:44 +0800

    LombokPlugin

---
+++ src/main/java/com/littmem/plugin/mybatis/generator/plugins/LombokPlugin.java
@@ -0,0 +1,44 @@
+package com.littmem.plugin.mybatis.generator.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author yang
+ * @version 18-6-23
+ */
+public class LombokPlugin extends PluginAdapter {
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+        topLevelClass.addAnnotation("@Data");
+        topLevelClass.addImportedType("lombok.Data");
+
+        return true;
+    }
+
+}
+
